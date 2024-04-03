72c2c842a38a3f3b9bd965b9dce6d6e65be0d0e1
Mousa <chammas_mousa@live.se>
2024-04-03 14:53:16 +0200

    removed registration, added template to index and layout

--- BankWeb/Pages/Index.cshtml.cs
+++ BankWeb/Pages/Index.cshtml.cs
@@ -1,20 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using DataAccessLayer.Models;
 
 namespace BankWeb.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly BankAppDataContext _dbContext;
+        public int TotalCustomers { get; set; }
+        public int TotalAccounts { get; set; }
+        public decimal TotalSum { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, BankAppDataContext dbContext)
         {
             _logger = logger;
-        }
+            _dbContext = dbContext;
 
+        }
         public void OnGet()
         {
-
+            TotalCustomers = _dbContext.Customers.Count();
+            TotalAccounts = _dbContext.Accounts.Count();
+            TotalSum = _dbContext.Transactions.Sum(t => t.Amount);
         }
     }
 }
